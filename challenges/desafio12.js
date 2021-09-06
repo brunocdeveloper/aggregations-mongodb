bdba31059592b7450a9b7ec7b42e93d4ec2e4af8
Bruno <brunocmoraes97@gmail.com>
2021-09-06 20:56:11 -0300

    Desafio 13

---
+++ challenges/desafio12.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      count: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $sort: {
+      count: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$count",
+    },
+  },
+]);
